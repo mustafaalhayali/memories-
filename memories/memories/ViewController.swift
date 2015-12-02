61e6f97dde93bd7f59a7ef065220232876890a65
spenceralderson <spenceralderson@gmail.com>
2015-12-02 13:59:20 -0500

    commit with parse ui elements

---
+++ memories/memories/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  memories
+//
+//  Created by Spencer Alderson on 2015-11-30.
+//  Copyright © 2015 spenceralderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewWillAppear(animated: Bool) {
+        if (PFUser.currentUser() == nil) {
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                
+                let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Login")
+                self.presentViewController(viewController, animated: true, completion: nil)
+            })
+        }
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ memories/memories/loginVC.swift
@@ -0,0 +1,16 @@
+//
+//  loginVC.swift
+//  memories
+//
+//  Created by Spencer Alderson on 2015-11-30.
+//  Copyright © 2015 spenceralderson. All rights reserved.
+//
+
+import UIKit
+
+
+class loginVC: UIViewController {
+    
+    
+
+}
